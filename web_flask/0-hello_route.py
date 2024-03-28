5b9fbd3938627b9a871cef2bbc4da5c1ee4c88df
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-28 00:08:02 +0000

    Hello HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Hello Flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello():
+	"""Method that returns Welcome message"""
+	return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+	app.run()
