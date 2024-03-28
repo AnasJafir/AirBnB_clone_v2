d9569890ee90cbd7885606edebc692a8f2017d51
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-27 17:17:22 -0700

    Hello HBNB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,13 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello():
-	"""Method that returns Welcome message"""
-	return "Hello HBNB!"
+    """Method that returns Welcome message"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
